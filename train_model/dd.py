cdf27ae6ef7e650b669b895132e6b1f51eead7b8
BC Gwak <kbckbc@gmail.com>
2022-11-09 21:42:10 -0600

    almost done

--- train_model/dd.py
+++ train_model/dd.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import numpy as np
 import pandas as pd
@@ -12,53 +13,71 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.svm import SVC
+# for dimension reduction
+from sklearn.feature_selection import SequentialFeatureSelector
 
 
 # for order by desc. return second element for sort
 def takeAccu(elem):
     return elem[1]
     
+
+# analyze on the input data set using several models 
 # data: dataframe of input file
-# type: kNN, RandomForest, SVM
-def training(data, model_type):
+# type: kNN, RandomForest, DecisionTree, SVM
+# problem: what to classfy
+# dimension_reduction: 'Y' yes, 'No' no
+def analyze(data, model_type, problem, dimension_reduction = 'N'):
+    
 
-    problem = [['H','K'], ['M','Y'], ['I','J']]
-    # problem = [, ['O','Q']]
     for p in problem:
-        print('Classify problem: {}'.format(p))
-
         # separate data into paramters(x) and a value(y)
         df = data.loc[data[0].isin(p)]
         x=df.iloc[:,1:]
         y=df.iloc[:,0]
+        print('Classification problem: {}'.format(p))
+        print('\tPicked data: x {}, y {}\n'.format(x.shape, y.shape))
 
-        if False: 
-            print('df\n{}'.format(df))
-            print('x shape: {}'.format(x.shape))
-            print('x\n{}'.format(x))
-            print('y shape: {}'.format(y.shape))
-            print('y\n{}'.format(y))
+        # for test
+        if False:
+            x=df.iloc[:100,1:]
+            y=df.iloc[:100,0]
 
+        ###################################
+        # dimension reduction
+        ###################################
+        if dimension_reduction == 'Y':
+            x = greedyBackward(model_type, x, y)
+
+        ###################################
         # set aside 10% for a test later
+        ###################################
         X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.1)   
 
-        # for kNN
-        # use 3 category hyperparameters, those are 'ball_tree','kd_tree','brute'
-        # use 10 number hyperparamters, 1 ~ 10
+        ###################################
+        # set category hyperparameters for each model
+        ###################################
         if model_type == 'kNN':
             categories = ['ball_tree','kd_tree','brute']
         elif model_type == 'RandomForest':
             categories = ['gini','entropy','log_loss']
         elif model_type == 'DecisionTree':
             categories = ['gini','entropy','log_loss']
+        elif model_type == 'SVM':
+            categories = ['rbf']
+
 
         for category in categories:
             k_scores = []
             hyper_score = []
             k_range = range(1, 11)
+            if model_type == 'SVM':
+                k_range = [0.001, 0.01, 0.02, 0.03, 0.05, 0.1, 0.5, 1, 5, 10]
 
-            # use iteration to caclulator different k in models, then return the average accuracy based on the cross validation
+            ###################################
+            # cross validation to find proper hyper parameter
+            ###################################            
             for k in k_range:
                 if model_type == 'kNN':
                     model = KNeighborsClassifier(algorithm=category, n_neighbors=k)
@@ -66,30 +85,26 @@ def training(data, model_type):
                     model = RandomForestClassifier(criterion=category, max_depth=k)
                 elif model_type == 'DecisionTree':
                     model = DecisionTreeClassifier(criterion=category, max_depth=k)
+                elif model_type == 'SVM':
+                    model = SVC(kernel=category, C=k)
 
-
-                # 
                 # do a Cross-validation test
                 scores = cross_val_score(model, X_train, y_train, cv=5)
                 k_scores.append(scores.mean())
                 if True:
-                    print('Testing: cross-vali, model: {}, category: {}, test hyper: {}, score: {}'.format(model_type, category, k, scores.mean()))
+                    print('Testing: cross-vali, model: {}, category haper: {}, number hyper: {}, score: {}'.format(model_type, category, k, scores.mean()))
 
                 hyper_score.append((k, scores.mean()))
 
-            # plot to see clearly
-            if False:
-                plt.plot(k_range, k_scores)
-                plt.xlabel('hyperparameter k (model:{} category: {})'.format(model_type, category))
-                plt.ylabel('Cross-Validated Accuracy')
-                plt.show()
-
+            ###################################
             # choose best hyperparameter from the hyper value we test above
             # pick the hyper parameter that yeilds the hightest score(or accuracy) 
             hyper_score.sort(key=takeAccu, reverse=True)
             bestHyper = hyper_score[0][0]
 
+            ###################################
             # test with a final validation set
+            ###################################
             start = time.time()
             if model_type == 'kNN':
                 model = KNeighborsClassifier(algorithm=category, n_neighbors=bestHyper)
@@ -97,15 +112,161 @@ def training(data, model_type):
                 model = RandomForestClassifier(criterion=category, max_depth=bestHyper)
             elif model_type == 'DecisionTree':
                 model = DecisionTreeClassifier(criterion=category, max_depth=bestHyper)            
-            model.fit(X_train, y_train)
+            elif model_type == 'SVM':
+                model = SVC(kernel=category, C=bestHyper)
+
+            model.fit(X_train, y_train)            
+            finalScore = model.score(X_test, y_test)
             end = time.time()
-            print('Result : final-vali, model: {}, category: {}, final hyper: {}, score: {}, time: {gap:.4f}\n'.format(model_type, category, bestHyper, model.score(X_test, y_test), gap = (end - start)))
+            print('Result : final-vali, model: {}, category hyper: {}, number hyper: {}, score: {}, time: {gap:.4f}\n'.format(model_type, category, bestHyper, finalScore, gap = (end - start)))
+
+            ###################################
+            # show plot to see clearly
+            ###################################
+            if True:
+                plt.plot(k_range, k_scores)                    
+                plt.title('{} {} - Reduced: {}, Categorical hyper: {}'.format(p, model_type, dimension_reduction, category))
+                plt.ylabel('Cross-Validated Accuracy')
+                if model_type == 'kNN':
+                    plt.xlabel('Numerical hyper: k(how many neighbors)')
+                elif model_type == 'RandomForest':
+                    plt.xlabel('Numerical hyper: k(max depth)')
+                elif model_type == 'DecisionTree':
+                    plt.xlabel('Numerical hyper: k(max depth)')
+                elif model_type == 'SVM':
+                    plt.xlabel('Numerical hyper: k(C, Regularization parameter)')
+                plt.show()
+
+# reduce dimension on the input x, y
+# model_type: 'kNN', 'RandomForest', 'DecisionTree', 'SVM'
+# x,y: input data
+# howmany: how many features do you want to left
+def greedyBackward(model_type, x, y, howmany = 4):
+    print('Greedy Backward is in progress.')
+    print('\tbefore x shape: {}'.format(x.shape))
+
+    if model_type == 'kNN':
+        model = KNeighborsClassifier()
+    elif model_type == 'RandomForest':
+        model = RandomForestClassifier(max_depth=8)
+    elif model_type == 'DecisionTree':
+        model = DecisionTreeClassifier(max_depth=8)
+    elif model_type == 'SVM':
+        model = SVC()
+
+    start = time.time()
+    sfs = SequentialFeatureSelector(model, direction='backward', n_features_to_select=howmany, cv = 5)
+    reduced_x = sfs.fit_transform(x,y)
+    end = time.time()
+
+    print('\tafter x shape: {}'.format(reduced_x.shape))
+    print('Reduced x: model {}, left features: {}, time: {gap:.4f}\n'.format(model_type, sfs.get_feature_names_out(), gap = (end - start)))
+
+    return reduced_x
+
+def usage(exec_name):
+    print('Usage:')
+    print('\tpython %s [arg1] [arg2] [arg3] [arg4]' % (exec_name))
+    print('\targ1: input data file')
+    print('\targ2: choose model. "kNN" "RandomForest" "DecisionTree" "SVM" are available')
+    print('\targ3: choose problem. 1:H,K 2:M,Y 3:I,J 4:H,K,M,Y,I,J') 
+    print('\targ4: apply dimension reduction. Y,y:yes N,n:no') 
+    print('\t\tGreedy Backward Feature Elimination will be applied')
+    print('Example:')
+    print('\tpython %s test.data kNN 1 N' % (exec_name))
+    print('\tpython %s test.data RandomForest 2 Y' % (exec_name))
+    print('\tpython %s test.data DecisionTree 3 N' % (exec_name))
+    print('\tpython %s test.data SVM 4 Y' % (exec_name))
+    exit()
+
+# check arguments and return arguments
+def checkarg(argv):
+    if len(argv) != 5:
+        usage(argv[0])
+    else :
+        model_type = argv[2]
+        problem = argv[3]
+        reduction = argv[4].upper()
+
+        if not (argv[2] == 'kNN' or argv[2] == 'RandomForest' or argv[2] == 'DecisionTree' or argv[2] == 'SVM'):
+            print('Error:')
+            print('\tcheck the argument [%s]' % (argv[2]))
+            usage(argv[0])
+
+        if not (argv[3] == '1' or argv[3] == '2' or argv[3] == '3' or argv[3] == '4'):
+            print('Error:')
+            print('\tcheck the argument [%s]' % (argv[3]))
+            usage(argv[0])
+
+        if not (argv[4] == 'Y' or argv[4] == 'N'):
+            print('Error:')
+            print('\tcheck the argument [%s]' % (argv[4]))
+            usage(argv[0])
+        
+        test_problem = []
+        if problem == '1':
+            test_problem = [['H','K']]
+        elif problem == '2':
+            test_problem = [['M','Y']]
+        elif problem == '3':
+            test_problem = [['I','J']]
+        elif problem == '4':
+            test_problem = [['H','K','M','Y','I','J']]
+
+        return [model_type, test_problem, reduction]
+
+
+if __name__ == '__main__':
+
+    # check program parameter
+    ret_arg = checkarg(sys.argv)
+    model_type = ret_arg[0]
+    problem = ret_arg[1]
+    reduction = ret_arg[2]
+
+    data=pd.read_csv(sys.argv[1],header=None)
+    print('Loading data from file [{}]'.format(sys.argv[1]))
+    print('\t Loaded data: {}\n'.format(data.shape))
+    
+    analyze(data, model_type, problem, reduction)
 
 
 
 
 
-def reduction(data):
+def reduce_analyze(data, model_type, problem):
+    for p in problem:
+        print('Classification problem: {}'.format(p))
+
+        # separate data into paramters(x) and a value(y)
+        df = data.loc[data[0].isin(p)]
+
+        x=df.iloc[:,1:]
+        y=df.iloc[:,0]
+        print('x shape: {}'.format(x.shape))
+        print('y shape: {}'.format(y.shape))
+
+        model = RandomForestClassifier(max_depth=8)
+        model.fit(x,y)
+        print('reduction1', model.score(x,y))
+
+        sfs = SequentialFeatureSelector(model, direction='backward', n_features_to_select=4, cv = 5)
+        z = sfs.fit_transform(x,y)
+
+        print('x shape: {}'.format(x.shape))
+        print('y shape: {}'.format(y.shape))
+        print('x {}'.format(x))
+        print('z {}'.format(z))
+        print('z {}'.format(sfs.get_feature_names_out()))
+
+
+        # # print('after', sfs.she)
+        model2 = RandomForestClassifier(max_depth=8)
+        model2.fit(z,y)
+        print('reduction2', model2.score(z,y))
+
+# my implement Greedy Backward Selection
+def myGreedyBack(data):
     # separate data into paramters(x) and a value(y)
     # df = data.loc[data[0].isin(['A'])]
     df = data
@@ -163,127 +324,5 @@ def reduction(data):
 
 
 
-if __name__ == '__main__':
-    debug = True
-    filename = 'test.data'
-    data=pd.read_csv(filename,header=None)
-
-    
-    # training(data, 'kNN')
-    # training(data, 'RandomForest')
-    training(data, 'DecisionTree')
-
-    # reduction(data)
 
 
-
-
-
-
-
-    '''
-    k_range = range(1, 11)
-    k_scores = []
-    # use iteration to caclulator different k in models, then return the average accuracy based on the cross validation
-    for k in k_range:
-        model = KNeighborsClassifier(n_neighbors=k)
-
-        fold = 0
-        skf = StratifiedKFold(n_splits=5)
-        fold_scores = []
-        for train_index, test_index in skf.split(X_train, y_train):
-            # print("TRAIN:", len(train_index), "TEST:", len(test_index))
-            train_x = X_train.iloc[train_index,:]
-            train_y = y_train.iloc[train_index]
-
-            test_x = X_train.iloc[test_index,:]
-            test_y = y_train.iloc[test_index]
-
-            model.fit(train_x, train_y)
-            fold_score = model.score(test_x, test_y)
-            fold_scores.append(fold_score)
-
-            if False:
-                print('fold: {}, accuracy: {}'.format(fold, fold_score))
-
-            fold += 1
-
-        k_score = model.score(X_test, y_test)
-        k_scores.append(k_score)
-        print('k: {}, cross accuracy: {}, test data accuracy: {}'.format(k, np.array(fold_scores).mean(), k_score))
-        # print('k: {}, test data accuracy: {}'.format(k, model.score(X_test, y_test)))
-
-    plt.plot(k_range, k_scores)
-    plt.xlabel('hyperparameter k')
-    plt.ylabel('Cross-Validated Accuracy')
-    plt.show()
-
-    '''
-
-
-
-    '''
-
-
-    print('a')
-
-    # print(df.head())
-    # print(df.isnull().sum())
-    # print(data[0].value_counts())
-
-    # df[0].value_counts().plot.bar()
-    # plt.plot()
-    # plt.show()
-
-
-    # print(x.head())
-    # print(y.head())
-    # print(x.shape)
-    # print(y.shape)
-
-    
-    # x = [[0, 1],
-    #    [2, 3],
-    #    [4, 5],
-    #    [6, 7],
-    #    [8, 9]]
-    # y = [0, 1, 2, 3, 4]
-
-    # z = df.loc[df[0].isin(['H','M'])]
-    # print(z)
-
-
-    # separate result into y and data into x
-    # df = data.loc[data[0].isin(['H','M'])]
-    # 
-    #     
-
-
-    # print(x_train)
-    # print(x_test)
-    # print(y_train)
-    # print(y_test)
-
-
-    
-    # knn = KNeighborsClassifier(algorithm='brute',n_neighbors =1 ,leaf_size=100,p=30)
-    knn = KNeighborsClassifier(n_neighbors =2)
-    # knn.fit(X_train, y_train)
-
-    # knn_predictions = knn.predict(X_test) 
-
-    # acc=accuracy_score(y_test,knn_predictions)
-    # print('Accuracy is :',acc*100)
-
-    # print('accuracy: ', knn.score(X_test, y_test))
-
-
-
-    # X,y will automatically devided by 5 folder, the scoring I will still use the accuracy
-    scores = cross_val_score(knn, X_train, y_train, cv=5, scoring='accuracy')
-    print(scores)
-    print(scores.mean())
-    
-
-
-    '''
